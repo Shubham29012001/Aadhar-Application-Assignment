675d6d0d5d4141c9916a2be00caa5ad305b3dd46
Shubham29012001 <smmaurya29012001@gmail.com>
2022-06-18 00:32:16 +0530

    Add App.js and Index.js Files

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,25 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Register from "./components/register/Register";
+import Edit from "./components/edit/Edit";
+import Detail from "./components/detail/Detail";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/edit/:id" element={<Edit />} />
+        <Route exact path="/view/:id" element={<Detail />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
