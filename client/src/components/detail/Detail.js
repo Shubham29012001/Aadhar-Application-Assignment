1a8c22890659f5d8f9c31919157176b3b63188bc
Shubham29012001 <smmaurya29012001@gmail.com>
2022-06-18 20:07:37 +0530

    Add Card Layout, Delete and Edit Functionality

--- client/src/components/detail/Detail.js
+++ client/src/components/detail/Detail.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React, useEffect, useState, useContext } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import "./Detail.css";
@@ -10,12 +10,19 @@ import HomeIcon from "@mui/icons-material/Home";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
 import BadgeIcon from "@mui/icons-material/Badge";
 import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink, useNavigate } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import { loginContext } from "../context/ContextProvider";
 
 const Detail = () => {
   const { id } = useParams("");
 
+  const [loginData, setloginData] = useContext(loginContext);
+  const userLoginID = loginData._id;
+
+  const history = useNavigate("");
   const [getUserData, setUserData] = useState([]);
+  const [getID, setID] = useState("");
 
   const getIndividualData = async () => {
     const res = await fetch(`http://localhost:8080/getIndividualData/${id}`, {
@@ -31,59 +38,99 @@ const Detail = () => {
       setUserData(dataResponse);
       console.log(dataResponse);
     } else {
-      alert(dataResponse.message);
       console.log(dataResponse.message);
     }
   };
 
   useEffect(() => {
     getIndividualData();
+    setID(userLoginID);
   }, []);
 
-  return (
-    <div className="container mt-3">
-      <h1 style={{ fontWeight: 400 }}>
-        Welcome {getUserData.first + " " + getUserData.last}
-      </h1>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <div className="row">
-            <div className="left_view col-12 col-lg-6 col-md-6">
-              <img src="/profile.png" style={{ width: 50 }} alt="profile" />
-            </div>
-            <div className="right_view col-12 col-lg-6 col-md-6">
-              <div className="add_btn">
-                <button className="btn btn-primary mx-2">
-                  <EditIcon />
-                </button>
-                <button className="btn btn-danger mx-2">
-                  <DeleteIcon />
-                </button>
-              </div>
-              <h3>
-                <BadgeIcon /> Name:{" "}
-                <span>{getUserData.first + " " + getUserData.last}</span>
-              </h3>
-              <h3>
-                <PhoneIphoneIcon /> Phone Number:{" "}
-                <span>{getUserData.phone}</span>
-              </h3>
-              <h3>
-                <MailOutlineIcon /> Email: <span>{getUserData.email}</span>
-              </h3>
-              <h3>
-                <HomeIcon /> Home Address: <span>{getUserData.address}</span>
-              </h3>
-              <h3>
-                <CreditCardIcon /> Aadhar Number:{" "}
-                <span>{getUserData.aadhar}</span>
-              </h3>
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  const deleteUser = async (id) => {
+    const deleteResponse = await fetch(
+      `http://localhost:8080/deleteUser/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const deleteData = await deleteResponse.json();
+
+    if (deleteResponse.status === 201) {
+      alert("Data Deleted Successfully");
+      history("/");
+    } else {
+      alert("Error Occured");
+    }
+  };
+
+  if (getID === id) {
+    return (
+      <div className="container mt-3">
+        <h1 style={{ fontWeight: 400 }}>
+          Welcome {getUserData.first + " " + getUserData.last}
+        </h1>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item xs={3}>
+            <Card sx={{ minWidth: 275, maxWidth: 700 }}>
+              <CardContent>
+                <div className="row">
+                  <div className="right_view col-12 col-lg-12 col-md-12">
+                    <div className="add_btn">
+                      <NavLink to={`/edit/${getUserData._id}`}>
+                        <button className="btn btn-primary mx-2">
+                          <EditIcon />
+                        </button>
+                      </NavLink>
+                      <button
+                        className="btn btn-danger mx-2"
+                        onClick={() => deleteUser(getUserData._id)}
+                      >
+                        <DeleteIcon />
+                      </button>
+                    </div>
+                    <h3>
+                      <BadgeIcon /> Name:{" "}
+                      <span>{getUserData.first + " " + getUserData.last}</span>
+                    </h3>
+                    <h3>
+                      <PhoneIphoneIcon /> Phone Number:{" "}
+                      <span>{getUserData.phone}</span>
+                    </h3>
+                    <h3>
+                      <MailOutlineIcon /> Email:{" "}
+                      <span>{getUserData.email}</span>
+                    </h3>
+                    <h3>
+                      <HomeIcon /> Home Address:{" "}
+                      <span>{getUserData.address}</span>
+                    </h3>
+                    <h3>
+                      <CreditCardIcon /> Aadhar Number:{" "}
+                      <span>{getUserData.aadhar}</span>
+                    </h3>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  } else {
+    return <h1>Unauthorized Access</h1>;
+  }
 };
 
 export default Detail;
