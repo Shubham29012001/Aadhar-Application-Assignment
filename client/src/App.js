9a5648afe77b2edd677b8fdeaf4c790f095310f3
Shubham29012001 <smmaurya29012001@gmail.com>
2022-06-18 20:08:42 +0530

    Adding Context and Protected Routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,17 +8,36 @@ import Home from "./components/home/Home";
 import Register from "./components/register/Register";
 import Edit from "./components/edit/Edit";
 import Detail from "./components/detail/Detail";
+import Error from "./components/error/Error";
+
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/login/Login";
+import { loginContext } from "./components/context/ContextProvider";
+
+import React, { useContext } from "react";
 
 function App() {
+  const [loginData, setloginData] = useContext(loginContext);
+
+  const userType = loginData.type;
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/edit/:id" element={<Edit />} />
-        <Route exact path="/view/:id" element={<Detail />} />
+        <Route exact path="/" element={<Login />} />
+        {userType === "admin" ? (
+          <>
+            <Route exact path="/admin" element={<Home />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/edit/:id" element={<Edit />} />
+          </>
+        ) : (
+          <>
+            <Route exact path="/view/:id" element={<Detail />} />
+            <Route exact path="/*" element={<Error />} />
+          </>
+        )}
       </Routes>
     </>
   );
